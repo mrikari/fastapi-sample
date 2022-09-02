eefdd887fd6cf896e21ea5d2735e052ecde643ca
mrikari <>
2022-09-03 01:13:23 +0900

    pytestを実行できる設定の追加

---
+++ app/tests/test_main.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_get_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "It works!"}
+
+
+def test_get_info():
+    response = client.get("/info")
+    assert response.status_code == 200
+    
+    res_json = response.json()
+    assert 'app_name' in res_json
+    assert 'app_version' in res_json
