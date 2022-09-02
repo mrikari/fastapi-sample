6fa3bdc8651279cca14c8dcfa48e8d33600546f3
mrikari <61904007+mrikari@users.noreply.github.com>
2022-09-03 00:55:21 +0900

    開発環境の整備 (#1)
    * 開発環境用ファイルの追加
    
    * 環境変数から設定を変更できるようにする
    
    * linterの設定
    
    Co-authored-by: mrikari <>

---
+++ app/config/settings.py
@@ -0,0 +1,10 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name = "FastAPI Sample"
+    app_version = "1.0.0"
+    debug = False
+
+    class Config:
+        env_file = ".env"
--- app/main.py
+++ app/main.py
@@ -1,12 +1,30 @@
-from fastapi import FastAPI
+from functools import lru_cache
+
+from config.settings import Settings
+from fastapi import Depends, FastAPI
 from fastapi.responses import JSONResponse
 
+conf = Settings()
 app = FastAPI(
-    title='FastAPI sample project.',
-    version='0.0.1',
+    title=conf.app_name,
+    version=conf.app_version,
+    debug=conf.debug,
 )
 
 
-@app.get('/')
+@lru_cache()
+def get_settings():
+    return Settings()
+
+
+@app.get("/")
 def root():
-    return JSONResponse(content={'message': 'Hello World.'})
+    return JSONResponse(content={"message": "It works!"})
+
+
+@app.get("/info")
+def get_info(settings: Settings = Depends(get_settings)):
+    return {
+        "app_name": settings.app_name,
+        "app_version": settings.app_version,
+    }
