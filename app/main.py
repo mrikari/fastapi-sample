25aa350de290ffa5ccac5e6d09f906971352164e
mrikari <>
2022-09-03 00:52:20 +0900

    環境変数から設定を変更できるようにする

--- app/main.py
+++ app/main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI
+from functools import lru_cache
+from fastapi import FastAPI, Depends
 from fastapi.responses import JSONResponse
+from config.settings import Settings
 
+conf = Settings()
 app = FastAPI(
-    title='FastAPI sample project.',
-    version='0.0.1',
+    title=conf.app_name,
+    version=conf.app_version,
+    debug=conf.debug,
 )
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
 
 
 @app.get('/')
 def root():
-    return JSONResponse(content={'message': 'Hello World.'})
+    return JSONResponse(content={'message': 'It works!'})
+
+
+@app.get('/info')
+def get_info(settings: Settings = Depends(get_settings)):
+    return {
+        "app_name": settings.app_name,
+        "app_version": settings.app_version,
+    }
