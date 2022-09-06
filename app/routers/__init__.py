dae762f8991b1775b26f93b05dae574af1ac2a00
mrikari <>
2022-09-07 01:27:34 +0900

    append省略

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -2,11 +2,5 @@ from fastapi.routing import APIRoute
 
 
 def override_route(base: list[APIRoute], override_routes: list[APIRoute]):
-    result = []
     override = set([(tuple(o.methods) + (o.path,)) for o in override_routes])
-    for b in base:
-        if tuple(b.methods) + (b.path,) in override:
-            pass
-        else:
-            result.append(b)
-    return result
+    return [b for b in base if tuple(b.methods) + (b.path,) not in override]
