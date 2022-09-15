50afe4608de840d5c72d70296584e8990ed995a9
mrikari <61904007+mrikari@users.noreply.github.com>
2022-09-16 00:36:24 +0900

    create_allしたときにschema以下をインポートする処理を省略するため追加 (#5)
    Co-authored-by: mrikari <>

--- app/database.py
+++ app/database.py
@@ -9,6 +9,8 @@ metadata = MetaData()
 
 
 def create_all():
+    import schemas  # noqa #F401
+
     engine = create_engine(
         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
     )
